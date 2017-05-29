9269bd9eb092f409ca8bdd133bf97313a3972783
Ramzitooo <osmar_sc2@live.com>
2017-05-29 17:28:05 -0300

    Modificacion Login - Simulador.
    -Poniendo en practica el Storage para la interactuar con los objetos.

--- Asistencia/src/app/app.module.ts
+++ Asistencia/src/app/app.module.ts
@@ -19,6 +19,9 @@ import { ListadoClasesProfesorPage } from '../pages/listado-clases-profesor/list
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IonicStorageModule } from '@ionic/storage';
+
+
 
 @NgModule({
   declarations: [
@@ -36,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
