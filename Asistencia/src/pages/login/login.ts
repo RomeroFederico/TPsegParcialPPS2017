8fe23e5ece12862fdf979b503064936debbc03a5
Ramzitooo <osmar_sc2@live.com>
2017-06-03 21:49:18 -0300

    Modificaciones Simples.

--- Asistencia/src/pages/login/login.ts
+++ Asistencia/src/pages/login/login.ts
@@ -1,136 +1,98 @@
 import { Component } from '@angular/core';
-import { AlertController, Platform, NavController } from 'ionic-angular';
-import { HomePage } from '../home/home';
-//import { RegisterPage } from '../register/register.ts';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { MenuPage } from '../menu/menu';
+import { Profesor } from '../../components/clases/profesor';
+import { Alumno } from '../../components/clases/alumno';
+import { Administrativo } from '../../components/clases/administrativo';
+import { Usuario } from '../../components/clases/usuario';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-    type= "password";
-    ios1="ios-eye";
-    ios2="md-eye";
-    show = false;
-    Pass = "password";
-    private todo : FormGroup;
-    imagen = "assets/img/fotoDefault.png";
-    unUsuario = {"email":"", "password":""};
 
-    constructor(public navCtrl: NavController,
-              public alertCtrl: AlertController,
-              private platform: Platform,
-              private formBuilder: FormBuilder) {
+  tipo:string = "Alumno";
+  profesor:Profesor = new Profesor(1,"Octavio","Villegas","99333222","100200","profesor@gmail.com","123",30,"profesor.png","Masculino");
+  alumno:Alumno = new Alumno(1,"Osmar","Flores","99333222","100200","alumno@gmail.com","123",30,"profesor.png","Masculino");
+  administrativo:Administrativo = new Administrativo(1,"Daniel","Gueler","99333222","100200","administrativo@gmail.com","123",30,"profesor.png","Masculino");
+  administrador:Usuario = new Usuario(1,"Federico","Romero","99333222","100200","administrador@gmail.com","123",30,"profesor.png","Masculino");
 
-    this.platform = platform;
-    this.Admin();
-    try
+  mail:string;
+  pass:string;
+
+  constructor(public navCtrl: NavController,private storage: Storage) 
+  {
+
+
+  }
+  Aceptar(tipo)
+  {
+    console.log("Login - Es de tipo: "+tipo);
+    
+    switch (tipo) 
     {
-      //CARGO LA BASE DE DATOS EN UNA VARIABLE
+      case 'Alumno':
+
+        this.tipo=tipo;
+        this.mail=this.alumno.email;
+        this.pass=this.alumno.password;
+        break;
+
+      case 'Profesor':
+        this.tipo=tipo;
+        this.mail=this.profesor.email;
+        this.pass=this.profesor.password;
+        break;
+
+      case 'Administrativo':
+        this.tipo=tipo;
+        this.mail=this.administrativo.email;
+        this.pass=this.administrativo.password;
+        break;
+      case 'Administrador':
+        this.tipo=tipo;
+        this.mail=this.administrador.email;
+        this.pass=this.administrador.password;
+        break;
     }
-    catch(e)
+
+  }
+  Login()
+  {
+    var tipo = this.tipo;
+    switch (tipo) 
     {
+      case 'Alumno':
 
+        this.alumno.email = this.mail;
+        this.alumno.password = this.pass;
+        this.storage.set("Alumno",this.alumno);
+        console.log(this.alumno);
+        break;
+
+      case 'Profesor':
+        this.profesor.email = this.mail;
+        this.profesor.password = this.pass;
+        this.storage.set("Profesor",this.profesor);
+        console.log(this.profesor);
+        break;
+
+      case 'Administrativo':
+        this.administrativo.email = this.mail;
+        this.administrativo.password = this.pass;
+        this.storage.set("Administrativo",this.administrativo);
+        console.log(this.administrativo);
+        break;
+      case 'Administrador':
+        this.administrador.email = this.mail;
+        this.administrador.password = this.pass;
+        this.storage.set("Administrador",this.administrador);
+        console.log(this.administrador);
+        break;
     }
-  }
-
-  toggleShow()
-  {
-        this.show = !this.show;
-        if (this.show){
-            this.type = "text";
-            this.ios1="ios-eye-off";
-            this.ios2="md-eye-off";
-        }
-        else {
-            this.type = "password";
-            this.ios1="ios-eye";
-            this.ios2="md-eye";
-        }
-  }
-
-  /*public CrearCuenta() {
-    this.navCtrl.push(RegisterPage);
-  }*/
-
-  public Admin() 
-  { 
-    this.todo = this.formBuilder.group({
-      email: ['admin@admin.com', Validators.required],
-      password: ['123456', Validators.required],
-    });
-  } 
-
-  public Alumno() 
-  {
-    this.todo = this.formBuilder.group({
-      email: ['alumno@alumno.com', Validators.required],
-      password: ['123456', Validators.required],
-    });
-  }
-
-  public Profesor() 
-  {
-   this.todo = this.formBuilder.group({
-      email: ['profesor@profesor.com', Validators.required],
-      password: ['123456', Validators.required],
-    });
-  }
-
-
-  public Administrativo() 
-  {
-    this.todo = this.formBuilder.group({
-      email: ['administrativo@administrativo.com', Validators.required],
-      password: ['123456', Validators.required],
-    });
-  }
-
-
-  Salir(): void
-  {
-      this.platform.exitApp();
-  }
-
-  CargarImagen(): void
-  {
-    this.imagen = "assets/img/fotoAdmin.jpg";
-  }
-
-  Login():void
-  {
-    let id = "";
-    let nombre = "";
-    let tipo = "";
-    try
-    {
-          if(true)//BUSCO EMAIL
-          {
-              if(true)//BUSCO PASS
-              {
-                  id = "1";
-                  this.unUsuario.email = "Admin@admin.com";
-                  nombre = "Admin";
-                  this.unUsuario.password = "123456";
-                  tipo = "administrador";
-                  this.navCtrl.setRoot(HomePage,{"usuario": {"id":id, "email":this.unUsuario.email, "nombre":nombre, "password":this.unUsuario.password, "vibrar":tipo}},{animate:true, direction:"forwart"});
-              }
-              else
-              {
-                  //INFORMO PASS INCORRECTA
-              }
-          }
-          else
-          {
-              //INFORMO NO SE ENCUENTRA EMAIL
-          }
-    }
-    catch(e)
-    {
-
-    }
+    this.navCtrl.setRoot(MenuPage,{Tipo:this.tipo});
   }
 
 }
