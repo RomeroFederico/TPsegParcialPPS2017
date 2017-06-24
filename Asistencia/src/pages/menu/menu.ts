28725318be8652e3b5d0fb92f2400c4a34ccfc08
RomeroFederico <romerof14@gmail.com>
2017-06-23 23:44:11 -0300

    Arreglado menu para Administrativo y Administrador

--- Asistencia/src/pages/menu/menu.ts
+++ Asistencia/src/pages/menu/menu.ts
@@ -21,6 +21,11 @@ import { NotificacionesAlumnoPage } from '../notificaciones-alumno/notificacione
 import { ListadoDivisionesAlumnoPage } from '../listado-divisiones-alumno/listado-divisiones-alumno';
 import { AsistenciaAlumnoPage } from '../asistencia-alumno/asistencia-alumno';
 
+import { ListadoAdministrativoPage } from '../listado-administrativo/listado-administrativo';
+import { DatosAdministrativoPage } from '../datos-administrativo/datos-administrativo';
+
+import { ListadoAdministradorPage } from '../listado-administrador/listado-administrador';
+
 @Component({
   providers:[Auth],
   selector: 'page-menu',
@@ -130,6 +135,67 @@ export class MenuPage {
   Mapa()
   {
     this.navCtrl.push(Mapa);
+  }
+
+  DatosAdministrativo()
+  {
+    this.navCtrl.push(DatosAdministrativoPage);
+  }
+
+  TomarAsistencia()
+  {
+    this.navCtrl.push(ListadoAdministrativoPage, {opciones : {tipo : "Asistencia", mensaje : "Tomar asistencia"}});
+  }
+
+  ListadoDivisionesAdministrativo()
+  {
+    this.navCtrl.push(ListadoAdministrativoPage, {opciones : {tipo : "Division", mensaje : "Listado de divisiones"}});
+  }
+
+  ListadoAlumnosAdministrativo()
+  {
+    this.navCtrl.push(ListadoAdministrativoPage, {opciones : {tipo : "Alumno", mensaje : "Listado de alumnos"}});
+  }
+
+  ListadoProfesoresAdministrativo()
+  {
+    this.navCtrl.push(ListadoAdministrativoPage, {opciones : {tipo : "Profesor", mensaje : "Listado de profesores"}});
+  }
+
+  ListadoUsuariosAdministrador()
+  {
+    this.navCtrl.push(ListadoAdministradorPage, {opciones : {tipo : "Usuario", mensaje : "Listado de usuarios"}});
+  }
+
+  ListadoDivisionesAdministrador()
+  {
+    this.navCtrl.push(ListadoAdministradorPage, {opciones : {tipo : "Division", mensaje : "Listado de divisiones"}});
+  }
+
+  ListadoMateriasAdministrador()
+  {
+    this.navCtrl.push(ListadoAdministradorPage, {opciones : {tipo : "Materia", mensaje : "Listado de materias"}});
+  }
+
+  ListadoAulasAdministrador()
+  {
+    this.navCtrl.push(ListadoAdministradorPage, {opciones : {tipo : "Aula", mensaje : "Listado de aulas"}});
+  }
+
+  NoImplementado()
+  {
+    let confirm = this.alertCtrl.create({
+      title: 'Informacion',
+      message: 'No implementado aun...',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
   
 
